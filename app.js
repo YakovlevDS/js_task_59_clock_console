f541b9fe9c81148fcdec9be37a2ea54e93f78ca6
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-09 16:26:27 +0200

    solution 2

--- app.js
+++ app.js
@@ -2,27 +2,35 @@
 
 
 // Solution 1
-  let cur_date = new Date();
-  let hours = cur_date.getHours();
-  let minutes = cur_date.getMinutes();
-  let seconds = cur_date.getSeconds();
+//   let cur_date = new Date();
+//   let hours = cur_date.getHours();
+//   let minutes = cur_date.getMinutes();
+//   let seconds = cur_date.getSeconds();
   
-const updateTime = () => {
+// const updateTime = () => {
 
-seconds += 1;
- if (seconds >= 60) {
-  minutes++;
-  seconds = 0;
- } 
- if (minutes >= 60) {
-  hours++;
-  minutes = 0;
- }  
-if (hours >= 24) { hours = 0 }
-console.log(`${hours} : ${minutes} : ${seconds}`);
-};
+// seconds += 1;
+//  if (seconds >= 60) {
+//   minutes++;
+//   seconds = 0;
+//  } 
+//  if (minutes >= 60) {
+//   hours++;
+//   minutes = 0;
+//  }  
+// if (hours >= 24) { hours = 0 }
+// console.log(`${hours} : ${minutes} : ${seconds}`);
+// };
 
-setInterval(updateTime, 1000);
+// setInterval(updateTime, 1000);
+
+// Solution 2
 
 
+  setInterval(() => console.log(
+   `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()}`
+ ), 1000); 
+
+
+// setInterval(updateTime, 1000);
 // ! Explanation: 
