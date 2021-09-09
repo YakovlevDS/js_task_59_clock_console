c392e0065199b39b68597867bfe55aaffb00590a
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-09 16:04:32 +0200

    added solution

---
+++ app.js
@@ -0,0 +1,28 @@
+// ? Task:Create a clock and display the time in each second
+
+
+// Solution 1
+  let cur_date = new Date();
+  let hours = cur_date.getHours();
+  let minutes = cur_date.getMinutes();
+  let seconds = cur_date.getSeconds();
+  
+const updateTime = () => {
+
+seconds += 1;
+ if (seconds >= 60) {
+  minutes++;
+  seconds = 0;
+ } 
+ if (minutes >= 60) {
+  hours++;
+  minutes = 0;
+ }  
+if (hours >= 24) { hours = 0 }
+console.log(`${hours} : ${minutes} : ${seconds}`);
+};
+
+setInterval(updateTime, 1000);
+
+
+// ! Explanation: 
